01dc20ca0a1866bff86636259a90a777a1cb5e12
meaarushi2002 <meaarushi2002@gmail.com>
2024-02-06 01:24:19 +0530

    made an imaginary car rentcalculation function

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utilities";
 import Image from "next/image";
 
@@ -22,15 +22,15 @@ export default async function Home() {
         </div>
         {!isDataEmpty ? (
           <section>
-            <div className="home__cars-wrapper">
-              {allCars?.map((car)=>(
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
                 <CarCard car={car} />
               ))}
             </div>
           </section>
-        ):(
-          <div className="home__error-container">
-            <h2 className="text-black text-xl font-bold">Oops,no results</h2>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
--- utilities/index.ts
+++ utilities/index.ts
@@ -3,9 +3,18 @@ export async function fetchCars() {
         'X-RapidAPI-Key': 'bb238318acmsh7366ccd8af711d7p193b53jsn42643f9b04b2',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',{
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',{
         headers: headers,
     });
     const result = await response.json();
     return result;
 } 
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; 
+    const mileageFactor = 0.1; 
+    const ageFactor = 0.05; 
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+    return rentalRatePerDay.toFixed(0);
+  };
