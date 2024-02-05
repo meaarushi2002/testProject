50dc82e16e0aedb2582aa82a5d24f54eeac56ad3
meaarushi2002 <meaarushi2002@gmail.com>
2024-02-05 19:16:03 +0530

    complete footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
+import { footerLinks } from "@/constants"
 
 const Footer = () => {
   return (
@@ -9,7 +10,30 @@ const Footer = () => {
           <Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain" />
           <p className="text-base text-gray-700">Carhub 2023 <br /> All rights reserved &copy;</p>
         </div>
+        <div className="footer__links">
+          {footerLinks.map((item)=>(
+            <div key={item.title} className="footer__link">
+              <h3 className="font-bold">{item.title}</h3>
+              {item.links.map((link)=>(
+                <Link key={link.title} href={link.url} className="text-gray-500">
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
+        <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+          <p>@2024 Carhub.All Rights Reserved</p>
+          <div className="footer__copyrights-link">
+            <Link href="/" className="text-gray-500">
+              Privacy Policy
+            </Link>
+            <Link href="/" className="text-gray-500">
+              Terms of Use
+            </Link>
+          </div>
+        </div>
     </footer>
   )
 }
