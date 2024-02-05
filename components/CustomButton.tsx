c7c6a0682ef6cb3e64afd073b0591ce68684ec77
meaarushi2002 <meaarushi2002@gmail.com>
2024-02-05 21:44:27 +0530

    adding searchbar and custom filter components

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const CustomButton = ({title, containerStyles, handleClick, btnType}:CustomButtonProps) => {
   return (
-    <button disabled={false} type={"button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+    <button disabled={false} type={btnType || "button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
       <span className={'flex-1'}> 
          {title}
       </span>
---
+++ components/SearchBar.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import SearchManufacturer from "./SearchManufacturer"
+
+const Searchbar = () => {
+  const handleSearch = () =>{
+
+  }
+  return (
+     <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
+     </form>
+  )
+}
+
+export default Searchbar
